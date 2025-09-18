ecd40f44cbd8250fc272ca5325cbad81b838e54f
Michael El Omar <michael.elomar@outlook.com>
2025-09-18 13:09:54 +0200

    create product.mk and config files

---
+++ rpi/config/product.mk
@@ -0,0 +1,12 @@
+TARGET_OS := linux
+TARGET_OS_FLAVOUR := generic
+TARGET_LIBC := eglibc
+
+TARGET_ARCH := aarch64
+TARGET_CPU := cortex-a72
+
+TARGET_CROSS := $(HOME)/Apps/arm-gnu-toolchain-14.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+TARGET_IMAGE_FORMAT := ext4
+TARGET_IMAGE_OPTIONS := --size=512M
+
+
